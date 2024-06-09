5d0789e2be0285af4f00935124812ec3b9d70b1b
Tantannn <tango.11@yahoo.com>
2024-06-09 17:15:50 +0700

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,14 +8,22 @@ return {
   {'ggandor/lightspeed.nvim'},
   --codeium AI
   { 'Exafunction/codeium.nvim',
-    event = 'BufEnter',
-    config = function ()
+    -- event = 'BufEnter',
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
       -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true }),
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true }),
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true }),
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+        })
     end
+    -- config = function ()
+    -- end
   },
   -- css color
   {'NvChad/nvim-colorizer.lua'},
