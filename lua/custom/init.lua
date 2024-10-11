d00186962a4123003ce29b54bb94cef4845ac650
Nm-Tan <tango.11@yahoo.com>
2024-10-11 15:34:39 +0700

    update gx

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -69,4 +69,16 @@ return {
   --     })
   --   end
   -- },
+  -- open browser
+  {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function ()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" }, -- Required for Neovim < 0.10.0
+    config = true, -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
+  }
 }
