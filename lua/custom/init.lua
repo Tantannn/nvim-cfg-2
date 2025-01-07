fc1d1f07aa2d8312576643bc34e6c49305c3122f
Nm-Tan <asdwzzz99@gmail.com>
2025-01-07 08:26:03 +0700

    reset

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,15 +6,16 @@
 return {
   { 'xiyaowong/transparent.nvim' },
   -- leap search
-  {
-    'ggandor/leap.nvim',
-    config = function()
-      require('leap').create_default_mappings()
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
-    end
-  },  -- git
+  -- {
+  --   'ggandor/leap.nvim',
+  --   config = function()
+  --     require('leap').create_default_mappings()
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+  --   end
+  -- }, 
+  -- git
   {'https://github.com/tpope/vim-fugitive',
     config = function()
       vim.keymap.set("n", "g<cr>", "<cmd>vertical Git<cr>")
